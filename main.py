dcdb63084735ca7948ec04cc0c6c84689439d5a8
cicerotcv <elm.tiago@gmail.com>
2022-03-08 19:22:43 -0300

    tokenizer classes

--- main.py
+++ main.py
@@ -1,78 +1,131 @@
 # -*- encoding: utf-8 -*-
 from sys import argv
-from typing import List
 
-OPERATORS = {
-    '+': lambda before, after: before + after,
-    '-': lambda before, after: before - after
-}
+# types
+T_INT = 'int'
+T_PLUS = 'plus'
+T_MINUS = 'minus'
+T_EOE = 'end_of_expression'
 
-
-def reduce(function, iterable, initial_value):
-    """reducer recursivo que aplica a função em cada 
-    elemento do iterável e usa o resultado como entrada da próxima iteração"""
-    if len(iterable) == 0:
-        return initial_value
-    current_value = function(initial_value, iterable[0])
-    return reduce(function, iterable[1:], current_value)
+# errors
+E_INITIAL_CHAR = 'initial_char'
+E_INVALID_TOKEN = 'invalid_token'
 
 
-def clean(expression: str):
-    return expression.replace(' ', '')
+class Error(Exception):
+    def __init__(self, code, message):
+        super().__init__({'code': code, 'message': message})
 
 
-def tokenize(expression: str):
-    """Quebra expressões em operadores e números"""
-    tokens = []
-    start = 0
-    length = len(expression)
-    for end in range(length):
-        if expression[end] in OPERATORS.keys():
-            tokens.append(expression[start:end])
-            tokens.append(expression[end])
-            start = end + 1
-        if end == length - 1:
-            tokens.append(expression[start:end+1])
-    return tokens
+class InvalidToken(Error):
+    def __init__(self, message) -> None:
+        super().__init__("invalid-token", message)
 
 
-def calculate(tokens: List[str]):
-    """Itera sobre a lista de tokens e faz operações entre os números"""
-    accumulator = 0
-    last_op = '+'
-    for token in tokens:
-        if token.isnumeric():
-            token = int(token)
-            function = OPERATORS[last_op]
-            accumulator = function(accumulator, int(token))
-        elif token in OPERATORS.keys():
-            last_op = token
-        else:
-            continue
-    return accumulator
+class InvalidExpression(Error):
+    def __init__(self, message) -> None:
+        super().__init__("invalid-expression", message)
 
 
-def safe_guard(case: str):
-    for char in case:
-        if not char.isnumeric() and char not in OPERATORS.keys():
-            raise Exception(f"Unrecognized char '{char}'")
+class Token:
+    def __init__(self, type, value=None):
+        self.type = type
+        self.value = value
 
-    if all([operator not in case for operator in OPERATORS.keys()]):
-        raise Exception("No operator found in the expression")
+    def __repr__(self):
+        if self.value != None:
+            return f'<{self.type}:{self.value}>'
+        return f'<{self.type}>'
 
-    if case.startswith('+'):
-        raise Exception("Expression starts with '+'")
+    def isNumber(self):
+        return self.type == T_INT
 
-    if case.endswith('+') or case.endswith('-'):
-        raise Exception("Expression ends with '-' or '+'")
+    def __add__(self, another):
+        if self.value != None:
+            return Token(T_INT, 10*int(self.value) + int(another.value))
 
-    return case
+
+class Tokenizer:
+    def __init__(self, origin: str):
+        self.origin: str = origin
+        self.position: int = 0
+        self.current: Token = None
+
+    def selectNext(self):
+
+        # self.current já consumiu o último caractere e self.origin
+        if self.position >= len(self.origin):
+            self.current = Token(T_EOE)
+            return self.current
+
+        # consumir espaços
+        while self.origin[self.position] == ' ':
+            self.position += 1
+            if self.position >= len(self.origin):
+                self.current = Token(T_EOE)
+                return self.current
+
+        if self.origin[self.position] == '+':
+            self.current = Token(T_PLUS)
+            self.position += 1
+            return self.current
+
+        if self.origin[self.position] == '-':
+            self.current = Token(T_MINUS)
+            self.position += 1
+            return self.current
+
+        if self.origin[self.position].isdigit():
+            candidate = ''
+
+            while self.origin[self.position].isdigit():
+                candidate += self.origin[self.position]
+                self.position += 1
+                if self.position >= len(self.origin):
+                    break
+
+            self.current = Token(T_INT, int(candidate))
+            return self.current
+
+        tokenValue = self.origin[self.position]
+        raise Exception(f"Unrecognized token '{tokenValue}'")
+
+
+class Parser:
+    tokens: Tokenizer = None
+
+    @staticmethod
+    def parseExpression():
+        tokens = Parser.tokens
+        accumulator = tokens.selectNext().value
+
+        while tokens.current.type != T_EOE:
+            currentToken = tokens.selectNext()
+            nextToken = tokens.selectNext()
+
+            if currentToken.type == T_EOE:
+                continue
+
+            if currentToken.type == T_PLUS and nextToken.type == T_INT:
+                accumulator += nextToken.value
+            elif currentToken.type == T_MINUS and nextToken.type == T_INT:
+                accumulator -= nextToken.value
+            else:
+                errorMessage = f"Invalid expression. {currentToken} {nextToken}"
+                raise InvalidExpression(errorMessage)
+
+        return accumulator
+
+    @staticmethod
+    def run(code):
+        Parser.tokens = Tokenizer(code)
+        return Parser.parseExpression()
 
 
 def main(case):
-    pipeline = [clean, safe_guard, tokenize, calculate]
-    rst = reduce(lambda value, f: f(value), pipeline, case)
-    print(rst)
+    result = Parser.run(case)
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
