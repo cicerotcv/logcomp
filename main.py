bb44359347efcdd1326496abf01a2d544e26429f
cicerotcv <elm.tiago@gmail.com>
2022-06-19 12:16:35 -0300

    asm file has the same name as input file

--- main.py
+++ main.py
@@ -17,3 +17,5 @@ if __name__ == '__main__':
     filepath = argv[1]
     content = load_file(filepath)
     main(content)
+    file_name, file_ext = filepath.split(".")
+    Nasm.dump(f'{file_name}.asm')
